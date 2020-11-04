2d32b36c8e019f51f5ac4458aaa81e0332f724ba
Christos Matskas <christos.matskas@microsoft.com>
2020-11-04 21:03:25 +0000

    changed auth model for terraform

--- main.tf
+++ main.tf
@@ -22,10 +22,6 @@ variable "client_secret" {
 }
 
 provider "azurerm" {
-  subscription_id = "d8011108-23b2-40d8-8bc4-1f3f77abe795"
-  client_id       = "8965d4fc-d1c2-4c29-adf2-3c15a41e49ea"
-  client_secret   = var.client_secret
-  tenant_id       = "e801a3ad-3690-4aa0-a142-1d77cb360b07"
   features {}
 }
 
@@ -47,5 +43,4 @@ resource "azurerm_mssql_database" "test" {
   read_scale     = true
   sku_name       = "BC_Gen5_2"
   zone_redundant = true
-
 }
